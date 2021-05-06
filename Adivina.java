50e2f6058d69be168190c5335049e9fd7f6520cc
jmonger02 <jmonger02@gmail.com>
2021-05-06 08:59:44 -0600

    subir codigo

---
+++ Adivina.java
@@ -0,0 +1,14 @@
+/**
+ * @author Liz, Rachel y Juan
+ * 
+ * @version 06/05/2021 * 
+ */
+public class Adivina {
+    private int numero;
+
+    public Adivina(int numero){
+        this.numero = numero;
+    }
+
+
+}
